d31393242b473fb020c11a092defbc76f22215a0
Ryan Hunter <36390187+systemsvanguard@users.noreply.github.com>
2018-02-12 04:25:54 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Quotations to Inspire
+
+**Quotes2Inspire** ~ Motivational Quotes to Inspire You.
+
+![Quotations to Inspire](http://ryanhunter.org/portfolio/quotes2inspire/quotes2inspire.jpg)
+
+---
+
+## Features
+- Angular.js framework with routing
+- Bootstrap 3
+- Fontastic.me custom generated vector icons
+- Google Fonts
+
+
+## License
+
+This project is licensed under the terms of the **MIT** license.
---
+++ quotes2inspire/quotes2inspire.js
@@ -0,0 +1,40 @@
+// generate random motivational quotes.  
+var app = angular.module("QuoteMachine", []);
+app.controller("quoteController", quoteController);
+app.factory("quoteFactory", quoteFactory);
+quoteFactory.$inject = ["$http"];
+function quoteFactory($http) {
+  return {
+    getQuotes: getQuotes
+  };
+  function getQuotes() {
+    return $http
+      .get(
+        "quotes2inspire.json"
+      )
+      .then(getQuotesComplete)
+      .catch(getQuotesFailed);
+  }
+  function getQuotesComplete(quotesBase) {
+    return quotesBase.data;
+    console.log("Loading quotes success");
+  }
+  function getQuotesFailed(error) {
+    console.log("Loading quotes failed" + error.data);
+  }
+}
+quoteController.$inject = ["quoteFactory"];
+function quoteController(quoteFactory) {
+  var vm = this;
+  vm.refresh = function() {
+    getQuotes();
+  };
+  getQuotes();
+  function getQuotes() {
+    return quoteFactory.getQuotes().then(function(data) {
+      vm.quote = data[Math.floor(Math.random() * data.length)];
+      return vm.quote;
+    });
+  }
+}
+
